a2311a52f65ae79ea6abc5f488c2415d6f9a3ba3
youngrichu <myabiti@gmail.com>
2026-03-14 16:41:16 +0400

    fix: reset newRole after dialog close, remove dead emailVerified hidden input, add migration statement-breakpoint separator

--- src/lib/server/db/migrations/0007_set_email_verified_default.sql
+++ src/lib/server/db/migrations/0007_set_email_verified_default.sql
@@ -1,2 +1,2 @@
-ALTER TABLE "user" ALTER COLUMN "email_verified" SET DEFAULT true;
+ALTER TABLE "user" ALTER COLUMN "email_verified" SET DEFAULT true;--> statement-breakpoint
 UPDATE "user" SET "email_verified" = true WHERE "email_verified" = false;
