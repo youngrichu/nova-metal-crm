869149a69c26eccc67e14f1a86143e4de0f19439
youngrichu <myabiti@gmail.com>
2026-03-16 22:55:22 +0400

    fix: reduce chart left padding to 32

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -235,7 +235,7 @@
             xScale={scaleTime()}
             y="revenue"
             yBaseline={0}
-            padding={{ top: 4, right: 4, bottom: 36, left: 36 }}
+            padding={{ top: 4, right: 4, bottom: 36, left: 32 }}
             tooltip={{ mode: 'bisect-x' }}
           >
             <Svg>
