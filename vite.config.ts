94c046e55e97e5d583d5aef2c535767b3e30bcdc
youngrichu <myabiti@gmail.com>
2026-03-16 19:18:36 +0400

    fix: add layerchart to ssr.noExternal to prevent .svelte SSR error

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,9 @@ import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig({ plugins: [paraglideVitePlugin({ project: './project.inlang', outdir: './src/lib/paraglide' }),tailwindcss(), sveltekit()] });
+export default defineConfig({
+  plugins: [paraglideVitePlugin({ project: './project.inlang', outdir: './src/lib/paraglide' }), tailwindcss(), sveltekit()],
+  ssr: {
+    noExternal: ['layerchart', '@layerstack/svelte-actions', '@layerstack/utils', '@layerstack/tailwind']
+  }
+});
