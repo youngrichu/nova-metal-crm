518587c1e20cdfce3a5e3a99ad6c1f7947dcc9aa
youngrichu <myabiti@gmail.com>
2026-03-17 13:04:23 +0400

    fix: add role="img" to NovaLogo SVG for explicit accessibility semantics

--- src/lib/components/ui/NovaLogo.svelte
+++ src/lib/components/ui/NovaLogo.svelte
@@ -7,7 +7,7 @@
 	});
 </script>
 
-<svg xmlns="http://www.w3.org/2000/svg" width="100%" viewBox="0 0 1536 672" class={className} aria-label="Nova Metal">
+<svg xmlns="http://www.w3.org/2000/svg" width="100%" viewBox="0 0 1536 672" class={className} role="img" aria-label="Nova Metal">
   <clipPath id="a-{uid}">
     <path d="M0 0h1535.625v672H0z"/>
   </clipPath>
